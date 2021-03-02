868f2170a2532056f0e65ed38f731be636fad4c0
lampi <lampi@192.168.1.78>
2021-03-02 07:52:20 -0600

    Commit para repocitorio

---
+++ src/primerproyectojava_66217/PrimerProyectoJava_66217.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava_66217;
+
+/**
+ *
+ * @author lampi
+ */
+public class PrimerProyectoJava_66217 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int edad= 22;
+        char letra= 'a';
+        boolean valorboolean = true;
+        System. out. println("edad = "+ edad);
+        System. out. println("letra = "+ letra);
+        System. out. println("valorboolean = "+ valorboolean);
+    }
+    
+}
